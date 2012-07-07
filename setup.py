cf9455dfd9082edf1d528ff88eb01c026bb8b43c
Friedrich Lindenberg <friedrich@pudo.org>
2012-07-07 15:55:56 +0200

    Python app skeleton

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='newsfacts',
+    version='0.1',
+    description="Tracking the numbers behind the news.",
+    long_description='',
+    classifiers=[
+        ],
+    keywords='fact-checking stories numbers journalism ddj',
+    author='Open Knowledge Foundation',
+    author_email='info@okfn.org',
+    url='http://okfn.org',
+    license='AGPLv3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=[],
+    include_package_data=False,
+    zip_safe=False,
+    install_requires=[
+        'sqlalchemy==0.7.8',
+        'Flask==0.9',
+        'Flask-Script==0.3.3',
+        'flask-sqlalchemy==0.16',
+        'Unidecode==0.04.9',
+        'python-dateutil==2.0'
+    ],
+    tests_require=[],
+    entry_points=\
+    """ """,
+)
